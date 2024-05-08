35fca25209f665921126bba444e10815b510a326
PencersGitHub <113054133+PencersGitHub@users.noreply.github.com>
2024-05-08 10:04:16 -0400

    Initial commit

---
+++ com/example/BubbleSort.java
@@ -0,0 +1,28 @@
+
+package com.example;
+
+/**
+ * Change me.
+ *
+ * @author Spencer Scarlett
+ * @version 1.0
+ * @since 2024-05-08
+ */
+
+// BubbleSort class
+public final class BubbleSort {
+
+    /** Private constructor to prevent instantiation. */
+    private BubbleSort() {
+        throw new UnsupportedOperationException("Cannot instantiate");
+    }
+
+    /**
+     * This is the main method.
+     *
+     * @param args Unused
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello, world");
+    }
+}
